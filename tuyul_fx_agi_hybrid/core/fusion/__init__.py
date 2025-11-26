08d2c0fa79e12d07ba3cb2d22b33858566b8e9b0
kadek tajox <Kadektjx@gmail.com>
2025-11-26 20:54:29 +0800

    Delete tuyul_fx_agi_hybrid/core/fusion directory
    Signed-off-by: kadek tajox <Kadektjx@gmail.com>

--- tuyul_fx_agi_hybrid/core/fusion/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-🐺 Fusion Core – TUYUL-KARTEL-FX-AGI-HYBRID v5.4.0
-Layer 8–12 Hybrid Fusion Engine
-"""
-
-from .hybrid_fusion_orchestrator_v540 import run_full_fusion_cycle
-
-__all__ = ["run_full_fusion_cycle"]
-__version__ = "5.4.0"

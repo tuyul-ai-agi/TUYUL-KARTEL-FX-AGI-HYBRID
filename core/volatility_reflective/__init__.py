8ccdeae57d8f9731cbfb4df1303c31918ab8cbdd
kadek tajox <Kadektjx@gmail.com>
2025-12-12 11:42:19 -0800

    Refactor VDD layer into reflective volatility package

---
+++ core/volatility_reflective/__init__.py
@@ -0,0 +1,13 @@
+"""Volatility reflective layer package."""
+
+from core.volatility_reflective.reflective_volatility_config_v573r import (
+    ReflectiveVolatilityConfig,
+)
+from core.volatility_reflective.reflective_volatility_engine import (
+    ReflectiveVolatilityEngine,
+)
+
+__all__ = [
+    "ReflectiveVolatilityConfig",
+    "ReflectiveVolatilityEngine",
+]
